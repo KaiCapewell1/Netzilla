8ad52b5476dbb80521b2f0366f54bc6fc79fed2b
Kai joshua Capewell <Kaijcapewell@gmail.com>
2025-11-05 20:59:52 +0000

    i Updated the print statement in get_data.py

--- get_data.py
+++ get_data.py
@@ -39,7 +39,8 @@ def Load_data_for_homepage(retries=3, timeout=10):
                         poster_url = f"{IMAGE_URL}{poster_path}"
                         print(f"Poster URL for {movie_name}: {poster_url}")
                     else:
-                        print("No poster available")
+                        
+                        print(f"No poster available for {movie_name}")
                     break
 
 
